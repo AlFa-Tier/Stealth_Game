79074d90842fed336cd87b19f0beba9584c6bcf8
Fadi Alhelo <fadi.alhelo2003@hotmail.com>
2025-01-01 14:02:52 +0100

    just minor stuff xD

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 from Inputs import Inputs
 
@@ -6,8 +7,15 @@ width,height = 1000,600
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Stealth Mini-game')
+# pygame.display.set_icon(icon)
+framerate = pygame.time.Clock()
 
+w,h = width/5,height/5
+test_surface = pygame.Surface((w,h))
+test_surface.fill("Red")
 
+"""
 while True:
     # process inputs
     
@@ -19,3 +27,16 @@ while True:
 
     # update everything
     pygame.display.update()
+    """
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        screen.blit(test_surface,(5,5))
+
+    #update everything
+    pygame.display.update()
+    framerate.tick(60)
